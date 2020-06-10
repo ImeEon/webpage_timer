1a8dc33caa2b64c464c8617db85ab220ef876413
ImeEon <66710543+ImeEon@users.noreply.github.com>
2020-06-10 11:45:35 +0800

    v1.1

--- src/script/visibility.js
+++ src/script/visibility.js
@@ -26,8 +26,6 @@ var onVisibilityChange = function(){
         }
         
     }
-    document.title=document[hiddenProperty];
-
 }
 
 var time=0;
@@ -41,36 +39,3 @@ var timing=setInterval(timer,1000);
 
 document.addEventListener(visibilityChangeEvent, onVisibilityChange);
 
-
-
-
-
-
-
-// var hidden,state,visibilityChange;
-
-// if(typeof document.hidden!=="undefined"){
-//     hidden="hidden";
-//     visibilityChange="visibilityChange";
-//     state="visibilityState";
-// } else if (typeof document.mozHidden !== "undefined") {
-// 	hidden = "mozHidden";
-// 	visibilityChange = "mozvisibilitychange";
-// 	state = "mozVisibilityState";
-// } else if (typeof document.msHidden !== "undefined") {
-// 	hidden = "msHidden";
-// 	visibilityChange = "msvisibilitychange";
-// 	state = "msVisibilityState";
-// } else if (typeof document.webkitHidden !== "undefined") {
-// 	hidden = "webkitHidden";
-// 	visibilityChange = "webkitvisibilitychange";
-// 	state = "webkitVisibilityState";
-// }
-
-// document.addEventListener("visibilityChange",function(){
-//     console.log("koko");
-//     document.title=document[hidden];
-// },false);
-
-// document.title=document[hidden];
-// console.log("koko");
